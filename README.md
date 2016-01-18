c160665efb356c5049bd3db1beffa9e542642785
Chinmoy Sharma <csharma@loyalty.com>
2016-01-18 16:39:58 -0500

    - Made the endpoint and the corresponding success code configurable.  - If not mentioned revert to default values. - Added test cases for all possible cases of endpoint and status codes.

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-# restify-health
+# restify-health-router
 
 A interface for restify that lets you perform a health check on your server.
 
+# Installation
+
+```bash
+$ npm install --save restify-health-router
+```
+
+# Example
+
+```javascript
+const healthRouterFactory = require('restify-health-router');
+const healthRouter = healthRouterFactory.create();
+
+healthRouter.applyRoutes(server);
+```
--- test/health.spec.js
+++ test/health.spec.js
@@ -2,28 +2,71 @@
 
 const restify = require('restify');
 const request = require('supertest');
-const Health = require('../lib/health');
-const HTTP = require('../test/test.data.js').HTTP;
+const health = require('../lib/health.router');
 
 describe('/health resource test', function () {
   let server;
 
+  let options;
+
   beforeEach(function () {
+    options = {
+      endpoint: '/tea',
+      successCode: 418
+    };
 
     server = restify.createServer();
-
-  });
-
-  it('Should hit /health and return 200 OK', function () {
-
-    Health().applyRoutes(server);
-
-    return request(server)
-        .get('/health')
-        .expect(HTTP.OPERATION_OK);
   });
 
   afterEach(function () {
     return server.close();
+  });
+
+  it(`Should default to /health endpoint and return specified status code: 418`, function () {
+    delete options.endpoint;
+
+    health(options).applyRoutes(server);
+
+    return request(server)
+      .get('/health')
+      .expect(418);
+  });
+
+  it(`Should use the mentioned endpoint /tea and return default 200 OK`, function () {
+    delete options.successCode;
+
+    health(options).applyRoutes(server);
+
+    return request(server)
+      .get('/tea')
+      .expect(200);
+  });
+
+  it(`Should use the mentioned endpoint /tea and return specified status code: 200 OK`, function () {
+
+    health(options).applyRoutes(server);
+
+    return request(server)
+      .get('/tea')
+      .expect(418);
+  });
+
+  it(`Should default to /health endpoint and return 200 OK`, function () {
+    delete options.endpoint;
+    delete options.successCode;
+
+    health(options).applyRoutes(server);
+
+    return request(server)
+        .get('/health')
+        .expect(200);
+  });
+
+  it(`Should default to /health endpoint and return 200 OK`, function () {
+    health().applyRoutes(server);
+
+    return request(server)
+      .get('/health')
+      .expect(200);
   });
 });
