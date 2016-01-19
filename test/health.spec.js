1feb95f0f32ca15715e35e4b403ab783002f9cfd
Chinmoy Sharma <csharma@loyalty.com>
2016-01-19 13:52:37 -0500

    Streamlining the tests.

--- test/health.spec.js
+++ test/health.spec.js
@@ -2,15 +2,26 @@
 
 const restify = require('restify');
 const request = require('supertest');
-const health = require('../lib/health.router');
+const createHealthRouter = require('../lib/health.router');
 
-describe('/health resource test', function () {
+const DEFAULT_OPTIONS = {
+  ENDPOINT: '/health',
+  SUCCESS_CODE: 200
+}
+
+describe('health.router', function () {
   let server;
 
   let options;
+  let expected;
 
   beforeEach(function () {
     options = {
+      endpoint: '/tea',
+      successCode: 418
+    };
+
+    expected = {
       endpoint: '/tea',
       successCode: 418
     };
@@ -22,51 +33,55 @@ describe('/health resource test', function () {
     return server.close();
   });
 
-  it(`Should default to /health endpoint and return specified status code: 418`, function () {
+  function endpointShouldReturnSuccessCode(options, expected) {
+    createHealthRouter(options).applyRoutes(server);
+
+    return request(server)
+      .get(expected.endpoint)
+      .expect(expected.successCode);
+  }
+
+  it('Should default to /health endpoint and return specified status code: 418', function () {
     delete options.endpoint;
 
-    health(options).applyRoutes(server);
+    expected.endpoint = DEFAULT_OPTIONS.ENDPOINT;
 
-    return request(server)
-      .get('/health')
-      .expect(418);
+    endpointShouldReturnSuccessCode(options, expected);
   });
 
-  it(`Should use the mentioned endpoint /tea and return default 200 OK`, function () {
+  it('Should use the configured endpoint /tea and return default 200 OK', function () {
     delete options.successCode;
 
-    health(options).applyRoutes(server);
+    expected.successCode = DEFAULT_OPTIONS.SUCCESS_CODE;
 
-    return request(server)
-      .get('/tea')
-      .expect(200);
+    endpointShouldReturnSuccessCode(options, expected);
   });
 
-  it(`Should use the mentioned endpoint /tea and return specified status code: 418 OK`, function () {
-
-    health(options).applyRoutes(server);
-
-    return request(server)
-      .get('/tea')
-      .expect(418);
+  it('Should use the mentioned endpoint /tea and return specified status code: 418', function () {
+    endpointShouldReturnSuccessCode(options, expected);
   });
 
-  it(`Should default to /health endpoint and return 200 OK`, function () {
+  it('Should default to /health endpoint and return 200 OK', function () {
     delete options.endpoint;
     delete options.successCode;
 
-    health(options).applyRoutes(server);
+    // FIXME(bbobnis): leverage DEFAULT_OPTIONS
+    expected.endpoint = '/health';
+    expected.successCode = 200;
 
-    return request(server)
-        .get('/health')
-        .expect(200);
+    endpointShouldReturnSuccessCode(options, expected);
   });
 
-  it(`Should return default values for endpoint and status code when options isn't present`, function () {
-    health().applyRoutes(server);
+  it('Should return default values for endpoint and status code when options isn\'t present', function () {
+    // FIXME(bbobnis): leverage DEFAULT_OPTIONS
+    expected.endpoint = '/health';
+    expected.successCode = 200;
 
-    return request(server)
-      .get('/health')
-      .expect(200);
+    endpointShouldReturnSuccessCode(null, expected);
+  });
+
+  // TODO(bbobnis): UNIT TEST the Router (router.get)
+  it.only('yolo', () => {
+    console.log(createHealthRouter.get());
   });
 });
